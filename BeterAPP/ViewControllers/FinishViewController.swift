c8e74bdb6827b4d9b69a4a6c8f4e2feaa57a68f4
1nvis1ble <alexeyzyamin@gmail.com>
2022-09-07 22:06:56 +0300

    моя ветка

---
+++ BeterAPP/ViewControllers/FinishViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FinishViewController.swift
+//  BeterAPP
+//
+//  Created by Aleksey Zyamin on 07.09.2022.
+//
+
+import UIKit
+
+class FinishViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
