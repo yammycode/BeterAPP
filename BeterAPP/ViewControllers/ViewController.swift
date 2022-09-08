49ff6a3af2103fce029fee46439922b6fc0b78a0
1nvis1ble <alexeyzyamin@gmail.com>
2022-09-08 22:25:59 +0300

    начало работы

--- BeterAPP/ViewControllers/ViewController.swift
+++ BeterAPP/ViewControllers/ViewController.swift
@@ -18,4 +18,4 @@ class ViewController: UIViewController {
 
 
 }
-// Mark: - UI
+
---
+++ BeterAPP/Views/HeaderHistoryTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  HistoryTableViewCell.swift
+//  BeterAPP
+//
+//  Created by Aleksey Zyamin on 08.09.2022.
+//
+
+import UIKit
+
+class HeaderHistoryTableViewCell: UITableViewCell {
+    
+    @IBOutlet var teamTwoImage: UIImageView!
+    @IBOutlet var teamOneImage: UIImageView!
+    
+    
+    @IBOutlet var teamOneLabel: UILabel!
+    @IBOutlet var teamTwoLabel: UILabel!
+    
+}
